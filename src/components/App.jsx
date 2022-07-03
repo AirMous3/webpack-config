10392d574ad9ee789043b2abd248f0915a41ddc2
AirMouse <airmouselol@gmail.com>
2022-07-03 10:15:02 +0300

    feature: add styled-components file-names support

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,20 @@
 import '../styles/index.scss';
 import {Counter} from './Counter';
+import styled from 'styled-components';
+import image from '@/assets/img.jpg';
+
+const Main = styled.div`
+  color: chartreuse;
+  font-size: 60px;
+  background-image: url(${image});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: contain;
+  min-height: 100vh;
+`;
 
 export const App = () => {
-    return <div className='container'>
+    return <Main>
         <Counter/>
-    </div>;
+    </Main>;
 };
